b28a5ae62502622cac198e919395fbe3fd9f520a
Vijay Subramani <Vijay@nfnlabs.in>
2021-11-11 23:01:01 +0530

    Initial Commit

---
+++ News app/News_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_appApp.swift
+//  News app
+//
+//  Created by Vijay on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct News_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
